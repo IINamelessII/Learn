0a65b1a36d0d0afaff581b28399a2058cadad464
Nitro <justnitro26@gmail.com>
2018-09-25 01:37:26 +0300

    GTS#0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Learn
+
+Ok, fuck it, i lied, its drum and bass, what you gona do?
+Also, I do what I want
---
+++ js/First.js
@@ -0,0 +1,5 @@
+'use strict';
+//alert("Press F5");
+function func1(elmnt) {
+  elmnt.style.color = (elmnt.style.color == "red") ? "blue" : "red";
+}
